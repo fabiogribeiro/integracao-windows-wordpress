d05e688d91fc061e46a1bd3eb02cd2484de1eee3
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2019-07-19 15:20:27 +0100

    Adicionar criacao de base dados e modelos associados

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,12 @@ namespace Integracao_Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        Modelo context = new Modelo();
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateTables();
         }
 
         private void Encomendas_Click(object sender, RoutedEventArgs e)
@@ -39,6 +44,38 @@ namespace Integracao_Windows
         private void NovoProduto_Click(object sender, RoutedEventArgs e)
         {
             mainFrame.NavigationService.Navigate(new NovoProduto());
+        }
+
+        /// <summary>
+        /// Cria tabelas na base dados de acordo com o modelo, caso estas não existam.
+        /// </summary>
+        private void CreateTables()
+        {
+            var cnn = context.Database.Connection;
+            cnn.Open();
+
+            var cmProdutos = cnn.CreateCommand();
+            cmProdutos.CommandText = "CREATE TABLE IF NOT EXISTS Encomendas ("+
+                                        "id         INTEGER NOT NULL UNIQUE," +
+                                        "Estado     TEXT," +
+	                                    "Endereco   TEXT," +
+	                                    "DataMod    INTEGER," +
+	                                    "PRIMARY KEY(\"id\")" +
+                                     ")";
+            cmProdutos.ExecuteNonQuery();
+
+            var cmEncomendas = cnn.CreateCommand();
+            cmEncomendas.CommandText = "CREATE TABLE IF NOT EXISTS Produtos ("+
+                                        "id         INTEGER NOT NULL UNIQUE," +
+                                        "Nome       TEXT," +
+	                                    "Preco      REAL," +
+	                                    "URLImagem  TEXT," +
+	                                    "DataMod    INTEGER," +
+	                                    "PRIMARY KEY(\"id\")" +
+                                     ")";
+            cmEncomendas.ExecuteNonQuery();
+
+            cnn.Close();
         }
     }
 }
